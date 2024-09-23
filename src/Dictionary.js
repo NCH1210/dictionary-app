54537c276d572223373ef4e8754d464eef92ac26
Nina Haide <nina.haide@gmail.com>
2024-09-23 10:03:00 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+  }
+  let apiKey = "to16932f9ae9f62c0a0504f6d6954afb";
+  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
+
+  function handleResponse() {
+    alert(`${keyword}`);
   }
 
   function handleKeywordChange(event) {
